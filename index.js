d230d93e8b7e272c23dd3f3dccc856c4ed85780d
Maradana hemalatha <hemamaradana5@gmail.com>
2026-04-28 18:04:40 +0530

    Fix: Render deploy issues

--- index.js
+++ index.js
@@ -302,7 +302,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-//
-server.on("error", (err) => {
-  console.error("SERVER ERROR:", err);
-});
+
